f01a0b6b9590f2f2c244d58da12a027df1328c4e
a-moseman <drewmoseman@gmail.com>
2024-03-10 14:45:04 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "glyphical's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs: {
+	nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+		system = "x86_64-linux";
+		modules = [
+			./configuration.nix
+		];
+	};
+  };
+}
