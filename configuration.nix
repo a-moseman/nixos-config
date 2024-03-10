bc73369f46c6500312c4278c909a37534901223d
a-moseman <drewmoseman@gmail.com>
2024-03-10 15:13:37 -0400

    git-credential-manager

--- configuration.nix
+++ configuration.nix
@@ -98,7 +98,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
 	git
-	git-credential-helper
+	git-credential-manager
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
