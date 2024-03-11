39a0b08f3a18c387bc62513ae97413c636b92746
a-moseman <drewmoseman@gmail.com>
2024-03-11 15:21:29 -0400

    Add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,20 @@
+#! /run/current-system/sw/bin/bash
+is_commit=false
+for i in "$@" ; do
+	if [[ $i == "-c" ]] ; then
+		is_commit=true
+		break
+	fi
+done
+# Verify valid arguments
+if [ "$is_commit" = true ] -a [ "$#" !+ 2 ] ; then
+	echo "Invalid arguments for commit"
+	exit 1
+fi
+# Rebuild and potentially commit
+sudo nixos-rebuild swtich
+if [ "$is_commit" = true ] ; then
+	git add *
+	git commit -m "$2"
+	git push
+fi
