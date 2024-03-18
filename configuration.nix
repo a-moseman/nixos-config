e75c2bac8f417933683e76b6df68c4c380201acc
a-moseman <drewmoseman@gmail.com>
2024-03-18 19:27:46 -0400

    Move time and locale configuration to time_locale.nix

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
 { config, pkgs, ... }: {
   	imports = [
 		./hardware-configuration.nix # Include the results of the hardware scan.
+		./time_locale.nix
 		./user.nix
 		./backups.nix
 		./laptop.nix
@@ -30,24 +31,6 @@
 
  	# Enable networking
 	networking.networkmanager.enable = true;
-
-	# Set your time zone.
-	time.timeZone = "America/New_York";
-
-	# Select internationalisation properties.
- 	i18n.defaultLocale = "en_US.UTF-8";
-
-  	i18n.extraLocaleSettings = {
-    		LC_ADDRESS = "en_US.UTF-8";
-    		LC_IDENTIFICATION = "en_US.UTF-8";
-    		LC_MEASUREMENT = "en_US.UTF-8";
-    		LC_MONETARY = "en_US.UTF-8";
-    		LC_NAME = "en_US.UTF-8";
-    		LC_NUMERIC = "en_US.UTF-8";
-    		LC_PAPER = "en_US.UTF-8";
-    		LC_TELEPHONE = "en_US.UTF-8";
-    		LC_TIME = "en_US.UTF-8";
-  	};
 
   	# Enable the X11 windowing system.
   	services.xserver.enable = true;
