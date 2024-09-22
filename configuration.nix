240fb5a0807974b6793cfc46ae477976bdfe5ec3
a-moseman <drewmoseman@gmail.com>
2024-09-22 16:57:48 -0400

    Add bismith window tiling manager

--- configuration.nix
+++ configuration.nix
@@ -90,6 +90,7 @@
 		git-credential-oauth
 		restic
 		libnotify
+		libsForQt5.bismuth
   		#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   		#  wget
   	];
