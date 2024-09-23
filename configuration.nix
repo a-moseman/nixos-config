a4bc08a91baba9f49caca333eb3ec3467bd77da4
a-moseman <drewmoseman@gmail.com>
2024-09-23 15:39:52 -0400

    Enable bluetooth and add ollama package

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,12 @@
 		./laptop.nix
 		./user.nix
 		./zsh.nix
+		./ollama.nix
     	];
 	
+	hardware.bluetooth.enable = true;
+	hardware.bluetooth.powerOnBoot = true;
+
 	virtualisation.docker.enable = true;
 
 	services.avahi = {
