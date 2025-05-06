c2e26e8056b6bc0dfdfe11a20a76558126fdc7f9
a-moseman <drewmoseman@gmail.com>
2025-05-05 21:29:20 -0400

    Add VSTs and such

--- packages/user/music.nix
+++ packages/user/music.nix
@@ -5,13 +5,13 @@ pkgs: with pkgs; [
 	#ardour
 	# plugins:
 	#dexed # <- doesn't work on ardour
-	#zam-plugins
-	#lsp-plugins
-	#tap-plugins
-	#distrho-ports # contains dexed
-	#infamousPlugins
-	#x42-plugins
-	#fmtoy
-	#oxefmsynth
-	#xsynth_dssi
+	zam-plugins
+	lsp-plugins
+	tap-plugins
+	distrho-ports # contains dexed
+	infamousPlugins
+	x42-plugins
+	fmtoy
+	oxefmsynth
+	xsynth_dssi
 ]
